9ac0266830859cd98bcd29258bf54268887458f3
santiagogalera30 <santiagogalera30@gmail.com>
2025-10-20 10:08:06 -0300

    BuffetEscolar
    Un proyecto con java para un buffet

---
+++ src/main/java/mz/BuffetEscolar/BuffetEscolarApplication.java
@@ -0,0 +1,13 @@
+package mz.BuffetEscolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffetEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuffetEscolarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/mz/BuffetEscolar/entity/Pedido.java
@@ -0,0 +1,80 @@
+package mz.BuffetEscolar.entity;
+
+import java.util.Date;
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPedido;
+
+    @Temporal(TemporalType.TIMESTAMP) 
+    private Date fecha;
+
+    private double total;
+
+    @ManyToOne(fetch = FetchType.LAZY) 
+    @JoinColumn(name = "id_usuario") 
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<DetallePedido> detalles;
+
+    public Pedido() {
+    }
+
+    public Integer getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public List<DetallePedido> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(List<DetallePedido> detalles) {
+        this.detalles = detalles;
+    }
+}
