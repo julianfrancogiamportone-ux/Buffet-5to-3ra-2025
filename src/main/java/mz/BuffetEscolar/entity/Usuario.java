66456313702bce66ae518c2f573e7272758a754b
santiagogalera30 <santiagogalera30@gmail.com>
2025-10-20 10:35:53 -0300

    Cambios santiagogalera
    Agregué las entidades

---
+++ src/main/java/mz/BuffetEscolar/entity/Usuario.java
@@ -0,0 +1,74 @@
+package mz.BuffetEscolar.entity;
+
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+
+    private String nombreUsuario;
+    private String contrasena; 
+    private String rol; 
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Pedido> pedidos;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombreUsuario, String contrasena, String rol) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+}
