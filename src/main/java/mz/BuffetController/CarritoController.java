48b195f5c6f3ad5869628969beb0a6e8e0717076
santiagogalera30 <santiagogalera30@gmail.com>
2025-11-10 22:59:37 -0300

    .
    .

--- src/main/java/mz/BuffetController/CarritoController.java
+++ src/main/java/mz/BuffetController/CarritoController.java
@@ -1,5 +1,45 @@
-package mz.BuffetEscolar.controller;
+package mz.BuffetController;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import mz.BuffetIService.IServiceCarrito;
+import mz.BuffetEntity.carrito;
+
+@RestController
+@RequestMapping("/personajes")
+@CrossOrigin(origins = "*")
 public class CarritoController {
+
+    @Autowired
+    private IServiceCarrito service;
+
+
+    @PostMapping
+    public carrito createCharacter(@RequestBody carrito carrito) {
+        return service.save(carrito);
+    }
+
+
+    @GetMapping
+    public List<carrito> getAllCharacters() {
+        return service.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public Optional<carrito> getCharacterById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
 
 }
--- src/main/java/mz/BuffetService/ServiceCarrito.java
+++ src/main/java/mz/BuffetService/ServiceCarrito.java
@@ -1,5 +1,36 @@
 package mz.BuffetEscolar.service;
 
-public class ServiceCarrito {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import mz.BuffetEscolar.Iservice.IServiceCarrito;
+import mz.BuffetEscolar.entity.carrito;
+import mz.BuffetEscolar.repository.CarritoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServiceCarrito implements IServiceCarrito {
+
+    @Autowired
+    private CarritoRepository repository;
+
+    @Override
+    public carrito save(carrito carrito) {
+        return repository.save(carrito);
+    }
+
+    @Override
+    public List<carrito> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<carrito> findById(Long id) {
+        return repository.findById(id);
+        
+    }
+
+   
 }
