a740705f7b30c9fe683dba9f225616b687154509
santiagogalera30 <santiagogalera30@gmail.com>
2025-11-18 13:24:21 -0300

    LISTO
    .

---
+++ src/main/java/mz/Buffet/BuffetAplication.java
@@ -0,0 +1,13 @@
+package mz.Buffet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffetAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuffetAplication.class, args);
+    }
+
+}
